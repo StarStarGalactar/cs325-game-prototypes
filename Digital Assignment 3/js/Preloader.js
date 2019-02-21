84519173661f6984fe9bdec3061748ae1ffbd639
StarStarGalactar <33713395+StarStarGalactar@users.noreply.github.com>
2019-02-20 23:35:21 -0500

    Update Preloader.js

--- Digital Assignment 3/js/Preloader.js
+++ Digital Assignment 3/js/Preloader.js
@@ -47,7 +47,7 @@ BasicGame.Preloader.prototype = {
 
 		//	Once the load has finished we disable the crop because we're going to sit in the update loop for a short while as the music decodes
 		//this.preloadBar.cropEnabled = false;
-		this.add.sprite(0, 0, 'loader');
+		
 	},
 
 	update: function () {
